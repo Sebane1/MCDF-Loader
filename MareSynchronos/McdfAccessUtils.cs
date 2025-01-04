0cbccde6e1bfcc1dfeab2b129fc00ff86e81e13a
Sebastina <sebane1@thavea.com>
2025-01-04 12:03:20 -0600

    Make sure cache is more aggressive in not reading from Program Files or FFXIV files location.

--- MareSynchronos/McdfAccessUtils.cs
+++ MareSynchronos/McdfAccessUtils.cs
@@ -11,7 +11,22 @@ namespace McdfDataImporter
     public static class McdfAccessUtils
     {
         private static string _cacheLocation = "";
-        public static string CacheLocation { get => _cacheLocation; set => _cacheLocation = value; }
+        public static string CacheLocation
+        {
+            get => _cacheLocation;
+            set
+            {
+                if (!string.IsNullOrEmpty(value) && (!value.Contains("Program Files")
+                 || !value.Contains("FINAL FANTASY XIV - A Realm Reborn")))
+                {
+                    _cacheLocation = value;
+                    if (!string.IsNullOrEmpty(_cacheLocation))
+                    {
+                        _cacheLocation = Path.Combine(Path.GetDirectoryName(_cacheLocation + ".poop"), "QuestCache\\");
+                    }
+                }
+            }
+        }
 
         public static IpcProvider McdfManager { get; set; }
     }
